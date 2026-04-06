460bb06f302a63381dc0bcd912fcebfa69705355
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-04-06 15:58:20 -0300

    mejorando

--- src/simple_project_generator/about.py
+++ src/simple_project_generator/about.py
@@ -1,6 +1,6 @@
 # about.py
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __package__ = "simple_project_generator"
 __program_name__ = "simple-project-generator"
 __author__ = "Fernando Pujaico Rivera"
