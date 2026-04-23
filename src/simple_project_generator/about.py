bf0e295057d5ac98e37127bc9889bf7dac33e56f
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-04-23 20:26:09 -0300

    ordenando

--- src/simple_project_generator/about.py
+++ src/simple_project_generator/about.py
@@ -1,11 +1,11 @@
 # about.py
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __package__ = "simple_project_generator"
 __program_name__ = "simple-project-generator"
 __author__ = "Fernando Pujaico Rivera"
 __email__  = "fernando.pujaico.rivera@gmail.com"
-__description__ = "my pypi project generator"
+__description__ = "My pypi project generator"
 __url_source__  = "https://github.com/trucomanx/SimpleProjectGenerator"
 __url_doc__  = "https://github.com/trucomanx/SimpleProjectGenerator/tree/main/doc"
 __url_funding__ = "https://trucomanx.github.io/en/funding.html"
